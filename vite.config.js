eb7fb8475d12b7d180cf54c4c9f6ce0ce93ab30b
Frédéric <hemery.f@gmail.com>
2026-04-03 16:35:47 +0200

    feat: enable PWA with manifest and Star Wars icon

--- vite.config.js
+++ vite.config.js
@@ -12,11 +12,48 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['icons/*.svg'],
-      manifest: false,
+      includeAssets: ['favicon.ico', 'icons/*.svg'],
+      manifest: {
+        name: 'StarWars Angers',
+        short_name: 'StarWars',
+        description: 'Application Star Wars - Donnees d\'Angers',
+        theme_color: '#ffd700',
+        background_color: '#0a0a12',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/icons/icon.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+            purpose: 'any maskable'
+          }
+        ]
+      },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365
+              }
+            }
+          },
+          {
+            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'gstatic-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365
+              }
+            }
+          },
           {
             urlPattern: /^https:\/\/data\.angers\.fr\/.*/i,
             handler: 'NetworkFirst',
